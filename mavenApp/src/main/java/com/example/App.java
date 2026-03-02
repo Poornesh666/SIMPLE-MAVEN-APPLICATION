74c3716f0bf3834ef6c479cd5ab1c53f2a0ee288
Poornesh666 <140740020+Poornesh666@users.noreply.github.com>
2026-03-02 22:13:30 +0530

    Update App.java

--- mavenApp/src/main/java/com/example/App.java
+++ mavenApp/src/main/java/com/example/App.java
@@ -1,1 +1,27 @@
+package com.example;
 
+public class App {
+
+    public String getGrade(int a) {
+
+        if (a < 0 || a > 100) {
+            return "Invalid Marks";
+        }
+
+        if (a >= 90) {
+            return "Grade S";
+        } else if (a >= 80) {
+            return "Grade A";
+        } else if (a >= 70) {
+            return "Grade B";
+        } else if (a >= 60) {
+            return "Grade C";
+        } else if (a >= 50) {
+            return "Grade D";
+        } else if (a >= 40) {
+            return "Grade E";
+        } else {
+            return "Grade F";
+        }
+    }
+}
